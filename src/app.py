feb78478aaf197d81d0006799ffbeaa5d6e28ae1
Himakar <himakar2005@gmail.com>
2025-02-06 23:45:12 +0530

    first commit

---
+++ src/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Chat with websites", page_icon="*")
+
+st.title("Chat with websites")
+
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
