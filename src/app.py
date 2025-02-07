0f2fcbeb292b1046f7fcf853a59ae4f6d34ed983
Himakar <himakar2005@gmail.com>
2025-02-07 14:24:14 +0530

    vector store

--- src/app.py
+++ src/app.py
@@ -1,31 +1,95 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever
+
+
+load_dotenv()
 
 def get_response(user_input):
     return "I dont know"
 
+def get_vectorstore_from_url(url):
+    # get the text in documents form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    # split the document into chunks
+    text_splitter= RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    #create a vectorstore from chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings() )
+
+    return vector_store 
+
+def get_context_retriever_chain(vector_store):
+    llm= ChatOpenAI()
+
+    retriever = vector_store.as_retriever()
+
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generates a search query to look up in order to information relevent")
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return retriever_chain
+
+
+
+
 #app config
-st.set_page_config(page_title="Chat with websites", page_icon="*")
+st.set_page_config(page_title="Chat with websites", page_icon="🌐")
 st.title("Chat with websites")
-chat_history = {
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
     AIMessage(content="Hello, I am a bot, How can i help you ?")
-}
+]
 
 # sidebar
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
+if website_url is None or website_url=="":
+    st.info("Please enter a website URL")
 
-#user input
-user_query =  st.chat_input("Type your message here.. ")
-if user_query is not None and user_query !="":
-    response=get_response(user_query)
-    chat_history.append(HumanMessage(content=user_query))
-    # with st.chat_message("Human"):
-    #     st.write(user_query)
-    # with st.chat_message("AI"):
-    #     st.write(response)
+else :
+    vector_store = get_vectorstore_from_url(website_url)
+
+    retriever_chain =get_context_retriever_chain(vector_store) 
+    
+
+    #user input
+    user_query =  st.chat_input("Type your message here.. ")
+    if user_query is not None and user_query !="":
+        response=get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+        retrieved_documents = retriever_chain.invoke({
+            "chat_history" : st.session_state.chat_history,
+            "input": user_query
+        })
+        st.write(retrieved_documents)
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
 
 
 
